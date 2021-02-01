1688555d188d842918097bb49b0042d47df0be2b
noblecloud <git@noblecloud.io>
2021-01-31 23:02:51 -0500

    Added basic support for multiple plots
    Signed-off-by: noblecloud <git@noblecloud.io>

--- widgets/Graph.py
+++ widgets/Graph.py
@@ -1,36 +1,38 @@
 import sys
 
 import numpy as np
-from matplotlib import cm
-from PIL import Image
-from pip._internal.utils.misc import enum
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtGui import QPainter, QPainterPath, QPen
-from PySide2.QtWidgets import QApplication, QHBoxLayout, QLabel
 
+from PySide2 import QtCore, QtGui, QtWidgets
+
+sys.modules['PyQt5.QtGui'] = QtGui
 from PIL.ImageQt import ImageQt
+from PySide2.QtGui import QBrush, QFont, QPainter, QPainterPath, QPen, QPixmap
+from PySide2.QtWidgets import QApplication, QHBoxLayout, QLabel
+from PIL import Image
+
+from math import sqrt
+from api.forecast import hourlyForecast
+
+golden = (1 + sqrt(5)) / 2
 
 
 class Graph(QtWidgets.QFrame):
-	time: np.ndarray
-	temp: np.ndarray
+	painter: QPainter
+	pix: QPixmap
+	_time: np.ndarray
+	_data: dict
 
-	def __init__(self, data, *args, **kwargs):
-		self._data = data
+	def __init__(self, *args, **kwargs):
+		# self._data = data
 		super(Graph, self).__init__(*args, **kwargs)
-		self.time: np.ndarray = data['timestampInt']
-		self.temp: np.ndarray = data['temp']
-		self.setStyleSheet('background-color: black;')
-		self.setContentsMargins(10, 20, 10, 20)
 		self.buildColorMaps()
-		self.backgroundImage()
+		self.setStyleSheet('background-color: black;')
+		self.font = QFont("SF Pro Rounded", 100)
+		self.setContentsMargins(10, 20, 10, 20)
 
 	def backgroundImage(self):
-
-		from PIL import Image
-		from matplotlib import cm
-
-		img = Image.fromarray(np.uint8(self.image())).convert('RGBA')
+		LightImage = self.image()
+		img = Image.fromarray(np.uint8(LightImage)).convert('RGBA')
 		img.resize((self.width(), self.height()), Image.ANTIALIAS)
 		qim = ImageQt(img)
 		pix = QtGui.QPixmap.fromImage(qim)
@@ -111,77 +113,95 @@ class Graph(QtWidgets.QFrame):
 		self.solarColorMap = LinearSegmentedColormap('colorMap', solarColorDict)
 
 	def paintEvent(self, event):
-
-		from scipy.constants import golden
-
 		self.margin = 0.2
 
+		self._time: np.ndarray = self.normalizeToFrame(self.data['timestampInt'], self.width())
+
 		# yStep = ((tempRange / y) - (y * self.margin))
-		paint = QPainter()
-		paint.begin(self)
+		self.painter = QPainter()
+		self.painter.begin(self)
 
-		paint.setRenderHint(QPainter.HighQualityAntialiasing)
-		paint.setRenderHint(QPainter.SmoothPixmapTransform)
-		paint.setRenderHint(QPainter.HighQualityAntialiasing)
+		self.painter.setRenderHint(QPainter.HighQualityAntialiasing)
+		self.painter.setRenderHint(QPainter.SmoothPixmapTransform)
 
-		temp = self.normalizeToFrame(self.temp, self.height(), 0.1)
-		time = self.normalizeToFrame(self.time, self.width())
+		self.painter.drawPixmap(self.rect(), self.pix)
 
-		paint.drawPixmap(self.rect(), self.pix)
-
-		lineThickness = self.height() * golden * 0.01
-		lineThickness = lineThickness if lineThickness > 8 else 8
-
-		pen = QPen(QtCore.Qt.white, lineThickness)
-		paint.setPen(pen)
-		path = QPainterPath()
-
-		path.moveTo(time[0], temp[0])
-
-		for xPlot, yPlot in zip(temp, time):
-			path.lineTo(yPlot, xPlot)
-		paint.drawPath(path)
 		babyBlue = QtGui.QColor(3, 232, 252)
-		pen.setColor(babyBlue)
-		paint.setPen(pen)
-		path = QPainterPath()
-		path.moveTo(0, temp[0] * self.height() / np.ptp(self.temp))
+		# self.painter.drawText(self.rect(), QtCore.Qt.AlignCenter, "Hello")
+		self.plotLine(self.data['feels_like'], babyBlue)
+		self.plotLine(self.data['temp'])
+		self.plotText(QtCore.QPoint(200, 200), 'test')
 		# for x in range(0, len(self.time)):
 		# 	y = (self.height() * 0.1) + ((self._data['feels_like'][x] - tmin) * yStep) * 0.8
 		# 	path.lineTo(x * xStep, y)
-		paint.drawPath(path)
-		paint.end()
+
+		self.painter.end()
+
+	def plotLine(self, data: np.ndarray, color=QtCore.Qt.white):
+		plotData = self.normalizeToFrame(data, self.height(), 0.1)
+
+		lineThickness = self.height() * golden * 0.01
+		lineThickness = lineThickness if lineThickness > 8 else 8
+		pen = QPen(color, lineThickness)
+		self.painter.setPen(pen)
+
+		path = QPainterPath()
+		path.moveTo(self._time[0], plotData[0])
+		for xPlot, yPlot in zip(plotData, self._time):
+			path.lineTo(yPlot, xPlot)
+
+		self.painter.drawPath(path)
+
+	def plotText(self, position: QtCore.QPoint, string: str, color=QtCore.Qt.white):
+		# lineThickness = self.height() * golden * 0.005
+		# lineThickness = lineThickness if lineThickness > .01 else 0.01
+		pen = QPen(color, 3)
+		brush = QBrush(QtCore.Qt.black)
+		self.painter.setPen(pen)
+		self.painter.setBrush(brush)
+		path = QPainterPath()
+		path.moveTo(position)
+		path.addText(position, self.font, string)
+		self.painter.drawPath(path)
+
+		pen = QPen(color, 0)
+		brush = QBrush(QtCore.Qt.black)
+		self.painter.setPen(pen)
+		self.painter.setBrush(brush)
+		path = QPainterPath()
+		path.moveTo(position)
+		path.addText(position, self.font, string)
+		self.painter.drawPath(path)
 
 	def normalizeToFrame(self, array, axis: int, offset: float = 0.0):
+
+		# start at leading margin
+		# subtract the lowest value
+		# height / peak to trough represents one unit value
+		# shrink by top margin
+
 		leading = axis * offset
+		if self.height() == axis:
+			return axis - (leading + (((array - array.min()) * axis / np.ptp(array)) * (1 - offset * 2)))
 		return leading + (((array - array.min()) * axis / np.ptp(array)) * (1 - offset * 2))
 
 	@property
 	def data(self):
 		return self._data
 
+	@data.setter
+	def data(self, data):
+		self._data = data
+		self.backgroundImage()
+
 	@staticmethod
 	def normalize(a):
 		return (a - np.min(a)) / np.ptp(a)
 
 	def image(self):
-		norm = self.normalize(self._data['surface_shortwave_radiation'])
-		raw = []
-		rS = 1.0
-		gS = 0.85
-		bS = 0.6
-		aS = 1.0
-		# for i in range(0, 272):
-		# 	r, g, b, a = [norm[i]] * 4
-		# 	r *= rS
-		# 	g *= gS
-		# 	b *= bS
-		# 	a *= 1
-		# 	raw.append((r))
-		raw = np.array(norm)
+		raw = self.normalize(self._data['surface_shortwave_radiation'])
 		raw = np.outer(np.ones(len(raw)), raw)
-		raw = np.flip(raw)
-		# raw = raw.reshape(272, 272, 3)
+		# raw = np.flip(raw)
 
 		raw = self.solarColorMap(raw)
 		scale = 1 / len(raw)
@@ -193,11 +213,6 @@ class Graph(QtWidgets.QFrame):
 		raw = raw.astype(np.uint8)
 
 		return raw
-
-	@data.setter
-	def data(self, value):
-		self.data = value
-		self.parseData()
 
 
 class GraphScene(QtWidgets.QGraphicsScene):
@@ -305,9 +320,16 @@ class GraphImage(QtWidgets.QFrame):
 if __name__ == '__main__':
 	app = QApplication()
 
-	from tests.pickler import pans as reload
-
-	data = reload('./tests/snapshots/202101031000')
+	# from tests.pickler import pans as reload
+	#
+	# data = reload('./tests/snapshots/202101031000')
+	data = hourlyForecast('hourly')
+	# data = hourlyForecast('hourly', measurementFields=['temp', 'precipitation', 'sunrise', 'sunset',
+	#                                                    'feels_like', 'dewpoint', 'precipitation_probability',
+	#                                                    'cloud_cover', 'surface_shortwave_radiation',
+	#                                                    'wind_speed',
+	#                                                    'epa_aqi', 'cloud_ceiling', 'cloud_base',
+	#                                                    'wind_direction'])
 	window = Graph(data)
 	window.show()
 
