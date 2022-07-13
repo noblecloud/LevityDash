17bce33341e87cdef914ea12963d251c53aa792e
noblecloud <git@noblecloud.io>
2022-07-13 12:49:30 -0400

    Slightly better error handling and better logging
    Signed-off-by: noblecloud <git@noblecloud.io>

--- src/LevityDash/lib/plugins/web/rest.py
+++ src/LevityDash/lib/plugins/web/rest.py
@@ -1,13 +1,17 @@
+import errno
 from typing import Optional
 
 import aiohttp
+from aiohttp import ClientOSError
 from rich.pretty import pretty_repr
 
 from LevityDash.lib.plugins.schema import LevityDatagram
 from LevityDash.lib.plugins.web import Endpoint, Web
-from LevityDash.lib.plugins.web.errors import APIError, InvalidCredentials, RateLimitExceeded
+from LevityDash.lib.plugins.web.errors import APIError, InvalidCredentials, RateLimitExceeded, RequestTimeout
 
 __all__ = ["REST"]
+
+from LevityDash.lib.plugins.errors import InvalidData
 
 
 class REST(Web, prototype=True):
@@ -16,9 +20,8 @@ class REST(Web, prototype=True):
 		async with aiohttp.ClientSession() as session:
 			async with session.get(url, params=params, headers=headers) as response:
 				if response.status == 200:
-					self.pluginLog.info(f'{self.name} request successful for {url}')
 					data = await response.json()
-					self.pluginLog.debug(pretty_repr(data, indent_size=2, max_width=120, max_depth=5, max_length=10, max_string=600))
+					self.pluginLog.verbose(pretty_repr(data, indent_size=2, max_width=120, max_depth=5, max_length=10, max_string=600), verbosity=5)
 					return await response.json()
 				elif response.status == 429:
 					self.pluginLog.error('Rate limit exceeded', response.content)
@@ -29,19 +32,10 @@ class REST(Web, prototype=True):
 				elif response.status == 404:
 					self.pluginLog.error(f'404: Invalid URL: {url}')
 				else:
-					self.pluginLog.error('API Error', response.content)
+					error = await response.text()
+					reason = await response.reason()
+					self.pluginLog.error('API Error', reason, error)
 					raise APIError(response)
-
-	# try:
-	# 	request = requests.get(url=url, params=params, headers=headers, timeout=10)
-	#
-	# 	# TODO: Add retry logic
-	# 	except aiohttp.client_exceptions.ClientConnectorError as e:
-	# 		self.pluginLog.error('ConnectionError')
-	# 		return
-	# 	except aiohttp.ServerTimeoutError as e:
-	# 		self.pluginLog.error(f'{self.name} request timed out for {url}')
-	# 		return
 
 	async def getData(self, endpoint: Endpoint, **kwargs) -> LevityDatagram:
 		if isinstance(endpoint, str):
@@ -58,16 +52,30 @@ class REST(Web, prototype=True):
 
 		try:
 			data = await self.__getData(url, params, headers)
-			datagram = LevityDatagram(data, schema=self.schema, sourceData={'endpoint': endpoint}, dataMap=self.schema.dataMaps.get(endpoint.name, {}))
-			self.pluginLog.verbose(f'{self.name} received parsed data: {datagram}')
-			return self.normalizeData(datagram)
+			datagram = LevityDatagram(
+				self.normalizeData(data),
+				schema=self.schema,
+				sourceData={'endpoint': endpoint},
+				dataMap=self.schema.dataMaps.get(endpoint.name, {})
+			)
+			if not len(datagram):
+				raise InvalidData(f'{self.name}\'s data {endpoint.name} request returned invalid data', endpoint)
+			self.pluginLog.verbose(f'{self.name}\'s {endpoint.name} request was successful', verbosity=0)
+			self.pluginLog.verbose(f'and received parsed data: {datagram}', verbosity=5)
+			return datagram
+		except ClientOSError as e:
+			if e.errno == errno.ETIMEDOUT:
+				self.pluginLog.error(f'{self.name}: {endpoint.name} request timed out for {url}')
+				self.pluginLog.exception(e)
+				raise RequestTimeout(f'{self.name}: {endpoint.name} request timed out')
+			raise APIError(f'{self.name} {endpoint.name} request failed for {url}')
 		except Exception as e:
-			self.pluginLog.error(f'{self.name} request failed for {url}')
+			self.pluginLog.error(f'{self.name} {endpoint.name} request failed for {url}')
 			self.pluginLog.exception(e)
-			return LevityDatagram({})
+			raise APIError(f'{self.name} {endpoint.name} request failed for {url}')
 
 	@property
 	def running(self):
-		from LevityDash.lib.plugins.plugin import ScheduledEvent
+		from LevityDash.lib.plugins.utils import ScheduledEvent
 		tasks = ScheduledEvent.instances.get(self, [])
 		return any(task.timer.when() for task in tasks)
