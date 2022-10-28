6bc8ce2f534fcace6f3c8d646c94d0de920d24c6
noblecloud <git@noblecloud.io>
2022-10-27 20:28:32 -0500

    feature(Errors): New Error types and new dynamic error creation
    Signed-off-by: noblecloud <git@noblecloud.io>

--- src/LevityDash/lib/plugins/errors.py
+++ src/LevityDash/lib/plugins/errors.py
@@ -1,3 +1,10 @@
+from typing import Type, TypeAlias, TypeVar, Union
+
+_E: TypeAlias = TypeVar('_E')
+
+_mixin_type_cache: dict[Type[Exception], Type['PluginError']] = {}
+
+
 class InvalidData(Exception):
 	pass
 
@@ -14,11 +21,34 @@ class PreferredSourceDoesNotHavePeriod(NoSourceForPeriod, NotPreferredSource):
 	pass
 
 
-class MissingPluginDeclaration(AttributeError):
+class PluginError(Exception):
+
+	@classmethod
+	def build_mixin_error(cls, error: _E) -> Type[Union[Type[_E], 'PluginError']]:
+		try:
+			return _mixin_type_cache[error]
+		except KeyError:
+			error_cls = type(error)
+			mixed = type(f'{cls.__name__}({error_cls.__name__})', (cls, error_cls), {})
+			_mixin_type_cache[error_cls] = mixed
+		return mixed
+
+	@classmethod
+	def mix_in_error(cls, error: _E) -> Union[Type[_E], 'PluginError']:
+		if isinstance(error, cls):
+			return error
+		return cls.build_mixin_error(error)
+
+
+class MissingPluginDeclaration(PluginError, AttributeError):
 	"""Raised when a plugin is missing the __plugin__ declaration."""
 	pass
 
 
-class PluginDisabled(Exception):
+class PluginDisabled(PluginError):
 	"""Raised when a plugin is loaded but is disabled."""
+	pass
+
+
+class PluginLoadError(PluginError, ImportError):
 	pass
