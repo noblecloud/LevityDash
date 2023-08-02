8c5cc4d89d79f1cb50353cfc73e54890e02c154f
noblecloud <git@noblecloud.io>
2023-08-02 11:30:28 -0400

    feature(UI.Gradient): Added support for generating conical gradients with start/stop angle and mapped values
    Signed-off-by: noblecloud <git@noblecloud.io>

--- src/LevityDash/lib/ui/colors/gradient.py
+++ src/LevityDash/lib/ui/colors/gradient.py
@@ -161,7 +161,7 @@ class Gradient(dict[str, MappedGradientValue[GradientValueType]]):
 		return list(cls.__presets__.keys())
 
 	@property
-	def list(self) -> List[MappedGradientValue[GradientValueType]]:
+	def as_list(self) -> List[MappedGradientValue[GradientValueType]]:
 		return sorted(list(self.values()), key=lambda x: x.value, reverse=False)
 
 	@property
@@ -173,7 +173,7 @@ class Gradient(dict[str, MappedGradientValue[GradientValueType]]):
 		return max(self, key=lambda x: x.value)
 
 	def __iter__(self):
-		return iter(self.list)
+		return iter(self.as_list)
 
 	def __get__(self, instance, owner):
 		from LevityDash.lib.ui.frontends.PySide.Modules.Displays.Graph import Plot
@@ -182,14 +182,147 @@ class Gradient(dict[str, MappedGradientValue[GradientValueType]]):
 		return self
 
 	@property
-	def valueRange(self):
-		return self.max - self.min
+	def valueRange(self) -> GradientValueType:
+		return self.max.value - self.min.value
 
 	def toQGradient(self, plot: 'Plot') -> QtGradient:
 		gradients = getOrSet(self.__dict__, '__QtGradients__', {})
 		if plot not in gradients:
 			gradients[plot] = self.QtGradient(plot, self)
 		return gradients[plot]
+
+	def gen_gradient_image(self, step_size: float = 0.1) -> QImage:
+		value_range = self.valueRange
+		min_value = self.min.value
+		if isinstance(value_range, Percentage):
+			step_size /= 100
+		width = int(value_range / step_size) + 2
+		height = 1
+		image = QImage(width, height, QImage.Format.Format_ARGB32)
+		image.fill(Qt.transparent)
+		painter = QPainter(image)
+		gradient = QLinearGradient(QPoint(0, 0), QPoint(width, 0))
+		for point in self.as_list:
+			color = point.color.QColor
+			value = float(point.value)
+			normalized_value = float(value - min_value) / float(value_range)
+			gradient.setColorAt(normalized_value, color)
+		painter.fillRect(image.rect(), gradient)
+		painter.end()
+		return image
+
+	@lru_cache(maxsize=512)
+	def get_color_for_value(self, value: GradientValueType) -> Color:
+		map = self.map
+		pixel_pos = round((float(value) - float(self.min.value)) / float(self.valueRange) * float(map.width()))
+		pixel_pos = max(0, min(pixel_pos, map.width()-1))
+		color = Color(map.pixelColor(pixel_pos, 0))
+		return color
+
+	@cached_property
+	def map(self) -> QImage:
+		return self.gen_gradient_image()
+
+	def toQConicalGradient(
+		self,
+		start_angle: float = 0,
+		stop_angle: float = 360,
+		min_value: float = None,
+		max_value: float = None,
+	) -> QConicalGradient:
+		"""
+		Converts the gradient to a QConicalGradient object.
+		Args:
+			start_angle (float): The starting angle of the gradient.
+			stop_angle (float): The stopping angle of the gradient.
+			min_value (float): The minimum value of the gradient.
+			max_value (float): The maximum value of the gradient.
+		Returns:
+			QConicalGradient: The converted QConicalGradient object.
+		"""
+		gradient = QConicalGradient()
+
+		inverted = float(start_angle) > float(stop_angle)
+		modifier = (lambda x: x) if inverted else (lambda x: 1-x)
+
+		min_angle, max_angle = sorted((start_angle, stop_angle))
+
+		full_angle = stop_angle - start_angle
+		mean_angle = min_angle + (full_angle / 2) * (1 if not inverted else -1)
+
+		# Sets the starting angle of the gradient to the between the start and stop angle
+		gradient.setAngle(-90 + -mean_angle)
+
+		own_value_cls = type(self.min.value)
+
+		# if no min or max value is provided, use the min and max values of the gradient
+		own_min_value = self.min.value
+		if min_value is None:
+			min_value = own_min_value
+		if not isinstance(min_value, own_value_cls):
+			min_value = own_value_cls(min_value)
+
+		own_max_value = self.max.value
+		if max_value is None:
+			max_value = own_max_value
+		if not isinstance(max_value, own_value_cls):
+			max_value = own_value_cls(max_value)
+
+		stops = self.as_list
+
+		if min_value != own_min_value:
+			stops.append(self.itemCls(min_value, self.get_color_for_value(min_value)))
+		if max_value != own_max_value:
+			stops.append(self.itemCls(max_value, self.get_color_for_value(max_value)))
+
+		stops.sort(key=lambda x: x.value)
+		values = np.array([float(point.value) for point in stops])
+
+		value_range = max_value - min_value
+
+		angle_scalar = float(abs(full_angle) / 360)
+
+		# Normalize the values to the new range of the gradient
+		normalized_values = (values - min_value) / value_range
+
+		# insure the values start at 0
+		normalized_values = normalized_values - (m_offset := normalized_values.min())
+
+		# scale the values to the full angle relative to a full circle
+		normalized_values = normalized_values * angle_scalar
+
+		# add the offset back to the values
+		normalized_values = normalized_values + (m_offset * angle_scalar)
+
+		# offset the values by half of the remaining angle
+		normalized_values = normalized_values + ((360 - abs(full_angle)) / 2 / 360)
+
+		# Add all the stops that are between 0.0 and 1.0 to the gradient
+		for point, item in zip(normalized_values, stops):
+			if 0 <= round(point, 6) <= 1:
+				gradient.setColorAt(modifier(point), item.color.QColor)
+
+		# if there are stops outside the range of the gradient, add stops at the edge of the gradient
+		value_per_degree = float(value_range / abs(full_angle))
+		padding = (360 - abs(full_angle)) / 2
+
+		if max_value < own_max_value and normalized_values.max() > 1:
+			gradient.setColorAt(
+				modifier(1),
+				self.get_color_for_value(max_value + padding * value_per_degree).QColor
+			)
+
+		if min_value > own_min_value and normalized_values.min() < 0:
+			gradient.setColorAt(
+				modifier(0),
+				self.get_color_for_value(min_value - padding * value_per_degree).QColor
+			)
+
+		return gradient
+
+	@cached_property
+	def stops(self) -> Tuple[GradientValueType]:
+		return sorted(list(self.values()), key=lambda x: x.value, reverse=False)
 
 	@classmethod
 	def representer(cls, dumper: SafeDumper, data):
